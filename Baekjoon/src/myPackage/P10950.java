3c83065d9c5bce78e95b75c8d3a537a24fced899
yoon.09 <use07143@daum.net>
2023-03-22 00:47:31 +0900

    10950

---
+++ Baekjoon/src/myPackage/P10950.java
@@ -0,0 +1,34 @@
+package myPackage;
+
+import java.util.Scanner;
+
+public class P10950 {
+	public static void main(String[] args) {
+		
+		int n;
+		
+		int a, b;
+		
+		int[] sum = null;
+		
+		Scanner keyboard = new Scanner(System.in);
+		n = keyboard.nextInt();
+		
+		sum = new int[n];
+		
+		for(int i = 0; i < n; i++) {
+			a = keyboard.nextInt();
+			b = keyboard.nextInt();
+			
+			sum[i] = a+b;
+		}
+		
+		for(int i = 0; i < n; i++) {
+			System.out.println(sum[i]);
+		}
+		
+		keyboard.close();
+		
+	}
+
+}

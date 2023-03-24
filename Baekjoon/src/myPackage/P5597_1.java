4d7512a52cdfba4c3c3399734616f187fd6c8381
yoon.09 <use07143@daum.net>
2023-03-25 05:40:01 +0900

    P5597

---
+++ Baekjoon/src/myPackage/P5597_1.java
@@ -0,0 +1,29 @@
+package myPackage;
+
+import java.io.*;
+
+public class P5597_1 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int[] arr = new int[30];
+		int n = 0;
+		
+		for(int i = 0; i < 28; i++) {
+			n = Integer.parseInt(br.readLine());
+			arr[n-1] = n;
+		}
+		
+		for(int i = 1; i < 31; i++) {
+			if(arr[i-1] == 0) bw.write(i + "\n");
+		}
+
+		
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+}

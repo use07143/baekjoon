9ef96980b7ce12fe7a382e0ee1f548a183879586
yoon.09 <use07143@daum.net>
2023-03-24 22:09:28 +0900

    P11718

--- Baekjoon/src/myPackage/P11718.java
+++ Baekjoon/src/myPackage/P11718.java
@@ -1,5 +1,23 @@
 package myPackage;
 
+import java.io.*;
+
 public class P11718 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	
+		String input = "";
+		
+		while(((input = br.readLine())!=null)&&(!input.isEmpty())) {
+			bw.write(input+"\n");
+		}
+		
+		br.close();
+		bw.flush();
+		bw.close();
+	}
 
 }

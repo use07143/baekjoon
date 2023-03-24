4d090481fe447f54825b6af4d5ec3f0ee8e415d7
yoon.09 <use07143@daum.net>
2023-03-24 19:00:23 +0900

    P2675

---
+++ Baekjoon/src/myPackage/P2675.java
@@ -0,0 +1,37 @@
+package myPackage;
+
+import java.io.*;
+import java.util.*;
+
+public class P2675 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	
+		int t = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st;
+		
+		for(int i = 0; i < t; i++) {
+			st = new StringTokenizer(br.readLine());
+			int n = Integer.parseInt(st.nextToken());
+			String s = st.nextToken();
+			
+			int len = 0;
+			while(len < s.length()) {
+				for(int k = 0; k < n; k++) {
+					bw.write(s.charAt(len));
+				}
+				len++;
+			}
+			bw.newLine();
+		}
+		
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+
+}

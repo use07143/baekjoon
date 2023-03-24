7bd2892d0bf8a5e803f5f446cfd5e29934aa8ff5
yoon.09 <use07143@daum.net>
2023-03-25 05:11:14 +0900

    P5597

---
+++ Baekjoon/src/myPackage/P5597.java
@@ -0,0 +1,44 @@
+package myPackage;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class P5597 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int[] arr = new int[28];
+		int a = 0, b = 0;
+
+		for(int i = 0; i < 28; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		
+		//Arrays.sort(arr);
+		
+		for(int i = 1; i <= 30; i++) {
+			boolean tf = Arrays.stream(arr)
+	                .boxed()
+	                .collect(Collectors.toSet())
+	                .contains(i);
+			if(!tf) {
+				if(a == 0) a = i;
+				else b = i;
+			}
+			
+		}
+		
+		
+		if (a < b) bw.write(a + "\n" + b);
+		else bw.write(b + "\n" + a);
+
+		
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+}

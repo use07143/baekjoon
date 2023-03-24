9d7972afcde43725f54fdee98be4ff23ecde5c28
yoon.09 <use07143@daum.net>
2023-03-25 00:33:42 +0900

    P10810

---
+++ Baekjoon/src/myPackage/P10810.java
@@ -0,0 +1,40 @@
+package myPackage;
+
+import java.util.*;
+import java.io.*;
+
+public class P10810 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer str;
+		str = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(str.nextToken());
+		int M = Integer.parseInt(str.nextToken());
+		
+		int[] arr = new int[N];
+		int i = 0, j = 0, k = 0;
+		
+		for(int x = 0; x < M; x++) {
+			str = new StringTokenizer(br.readLine());
+			i = Integer.parseInt(str.nextToken());
+			j = Integer.parseInt(str.nextToken());
+			k = Integer.parseInt(str.nextToken());
+			
+			for(int y = i-1; y < j; y++) {
+				arr[y] = k;
+			}
+		}
+		
+		for(int x = 0; x < N; x++) {
+			bw.write(arr[x] + " ");
+		}
+		
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+}

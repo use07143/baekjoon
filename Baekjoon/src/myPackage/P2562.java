b3e642c7820b8fba35facbd75056369068737635
yoon.09 <use07143@daum.net>
2023-03-24 23:53:52 +0900

    P2562

---
+++ Baekjoon/src/myPackage/P2562.java
@@ -0,0 +1,29 @@
+package myPackage;
+
+import java.io.*;
+
+public class P2562 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int num;
+		int count = 0, max = 0;
+		
+		for(int i = 0; i < 9; i++) {
+			num = Integer.parseInt(br.readLine());
+			if(num > max) {
+				max = num;
+				count = i+1;
+			}
+		}
+		
+		bw.write(max+"\n"+count);
+		
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+}

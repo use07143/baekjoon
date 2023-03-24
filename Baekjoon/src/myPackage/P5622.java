83b641cf5bb1d5d537c3a14a2e11288d2b0d96ba
yoon.09 <use07143@daum.net>
2023-03-24 22:09:03 +0900

    P5622

--- Baekjoon/src/myPackage/P5622.java
+++ Baekjoon/src/myPackage/P5622.java
@@ -1,31 +1,55 @@
 package myPackage;
 
 import java.io.*;
-import java.util.*;
 
 public class P5622 {
 
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		String str = br.readLine();
 		
-		int t = 0;		
+		int time = 0;		
 
 		for(int i = 0; i < str.length(); i++) { 
-			int temp = (int)(str.charAt(i) - 'A')/3 + 3;
-			if(str.charAt(i) == 'Z') temp -= 1;
-		 
-			t += temp; 
+			switch(str.charAt(i)) {
+			case 'A': case 'B': case 'C':
+				time += 3;
+				break;
+				
+			case 'D': case 'E': case 'F':
+				time += 4;
+				break;
+			
+			case 'G': case 'H': case 'I':
+				time += 5;
+				break;
+				
+			case 'J': case 'K': case 'L':
+				time += 6;
+				break;
+				
+			case 'M': case 'N': case 'O':
+				time += 7;
+				break;
+				
+			case 'P': case 'Q': case 'R': case 'S':
+				time += 8;
+				break;
+				
+			case 'T': case 'U': case 'V':
+				time += 9;
+				break;
+				
+			case 'W': case 'X': case 'Y': case 'Z':
+				time += 10;
+				break;
+			}
 		}
 		 
-		bw.write(t+"\n");
+		System.out.println(time);
 		
 		br.close();
-		bw.flush();
-		bw.close();
 	}
-
 }

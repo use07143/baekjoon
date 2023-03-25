783a2d1551c512e5ea43ad89843b4f6339ee4d29
yoon.09 <use07143@daum.net>
2023-03-26 03:20:52 +0900

    P2738

---
+++ Baekjoon/src/myPackage/P2738.java
@@ -0,0 +1,51 @@
+package myPackage;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class P2738 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st;
+		st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		int[][] arr1 = new int[N][M];
+		int[][] arr2 = new int[N][M];
+		int sum;
+		
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j = 0; j < M; j++) {
+				arr1[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j = 0; j < M; j++) {
+				arr2[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < M; j++) {
+				sum = arr1[i][j] + arr2[i][j];
+				bw.write(sum + " ");
+			}
+			bw.newLine();
+		}
+		
+
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+}

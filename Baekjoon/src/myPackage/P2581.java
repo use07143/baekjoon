7e7334d3251c9891bc0993c167a67470e7b0a5b4
yoon.09 <use07143@daum.net>
2023-03-26 01:57:01 +0900

    P2581

---
+++ Baekjoon/src/myPackage/P2581.java
@@ -0,0 +1,46 @@
+package myPackage;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+
+public class P2581 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine());
+		int M = Integer.parseInt(br.readLine());
+		
+		int sum = 0, min = Integer.MAX_VALUE, prime;
+		
+		for(int i = N; i <= M; i++) {
+			prime = 1;
+			
+			if(i == 1) continue;
+			
+			for(int j = 2; j < i; j++) {
+				if(i % j == 0) {
+					prime = 0;
+					break;
+				}
+			}
+			
+			if(prime == 1) {
+				sum += i;
+				if(min > i) min = i;
+			}
+		}
+		
+		if(sum == 0) bw.write(-1 + "");
+		else bw.write(sum + "\n" + min);
+		
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+}

53097d12113336a249744d1739f864a81d3fa3c5
yoon.09 <use07143@daum.net>
2023-03-25 09:27:57 +0900

    P10988

---
+++ Baekjoon/src/myPackage/P10988.java
@@ -0,0 +1,27 @@
+package myPackage;
+
+import java.io.*;
+
+public class P10988 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		String str = br.readLine();
+		int n = str.length();
+		
+		int f = 1;
+		
+		for(int i = 0; i < (n/2); i++) {
+			if(str.charAt(i) != str.charAt(n-i-1)) f = 0;
+		}
+		
+		bw.write(f + "");
+		
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+}

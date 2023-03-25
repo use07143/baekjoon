545b5ef76211ab7012396c10bdea2773fc8cefff
yoon.09 <use07143@daum.net>
2023-03-26 00:41:54 +0900

    P1316

---
+++ Baekjoon/src/myPackage/P1316.java
@@ -0,0 +1,38 @@
+package myPackage;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+
+public class P1316 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int n = Integer.parseInt(br.readLine());
+		int count = 0;
+		String str = "";
+		int v = 1;
+		
+		for(int i = 0; i < n; i++) {
+			str = br.readLine();
+			for(int j = 0; j < str.length()-1; j++) {
+				if(str.charAt(j) != str.charAt(j+1)) {
+					if(str.lastIndexOf(str.charAt(j)) != j) v = 0;
+				}
+			}
+			if(v == 1) count++;
+			v = 1;
+		}
+		
+		bw.write(count + "");
+		
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+}

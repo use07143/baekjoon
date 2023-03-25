e83a2f3fca1ca7d18aa1d7745f98795df1143947
yoon.09 <use07143@daum.net>
2023-03-26 03:03:46 +0900

    P2566

---
+++ Baekjoon/src/myPackage/P2566.java
@@ -0,0 +1,46 @@
+package myPackage;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class P2566 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int[][] arr = new int[9][9];
+		int x = 0, y = 0;
+		int max = -1;
+		
+		StringTokenizer st;
+		
+		for(int i = 0; i < 9; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j = 0; j < 9; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		for(int i = 0; i < 9; i++) {
+			for(int j = 0; j < 9; j++) {
+				if(arr[i][j] > max) {
+					max = arr[i][j];
+					x = j+1;
+					y = i+1;
+				}
+			}
+		}
+		
+		bw.write(max + "\n" + y + " " + x);
+
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+}

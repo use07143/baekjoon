8a465d2f9d9e883b80abf44224ce55d6dd369bfb
yoon.09 <use07143@daum.net>
2023-03-26 22:30:27 +0900

    P1026

---
+++ Baekjoon/src/myPackage/P1026.java
@@ -0,0 +1,44 @@
+package myPackage;
+
+import java.io.*;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class P1026 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		
+		int N = Integer.parseInt(br.readLine());
+		int[] A = new int[N];
+		int[] B = new int[N];
+		int sum = 0;
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) {
+			A[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) {
+			B[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(A);
+		Arrays.sort(B);
+		
+		for(int i = 0; i < N; i++) {
+			sum += (A[i] * B[N-i-1]);
+		}
+		
+		bw.write(sum + "");
+		
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+}

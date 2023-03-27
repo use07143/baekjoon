e8c1b8c43f198b642a81106ba42906efa46a9c8c
yoon.09 <use07143@daum.net>
2023-03-27 12:37:00 +0900

    P1735

---
+++ Baekjoon/src/myPackage/P1735.java
@@ -0,0 +1,46 @@
+package myPackage;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class P1735 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		int n1 = Integer.parseInt(st.nextToken());
+		int m1 = Integer.parseInt(st.nextToken());
+		
+		st = new StringTokenizer(br.readLine());
+		int n2 = Integer.parseInt(st.nextToken());
+		int m2 = Integer.parseInt(st.nextToken());
+		
+		int lcd = LCD(Math.min(m1, m2), Math.max(m1, m2));
+		
+		int n = n1*(lcd/m1) + n2*(lcd/m2);
+		int gcd = GCD(Math.min(n, lcd), Math.max(lcd, n));
+		
+		n /= gcd;
+		lcd /= gcd;
+		
+		bw.write(n + " " + lcd);
+		
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+	
+	public static int GCD (int a, int b) {
+		if(b == 0) return a;
+		else return GCD(b, a % b);
+	}
+	
+	public static int LCD (int a, int b) {
+		return a*b/GCD(a,b);
+	}
+}

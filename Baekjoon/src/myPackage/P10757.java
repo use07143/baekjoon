17cbfab09ed00af6a31c58b18d46e6b0fd97f0de
yoon.09 <use07143@daum.net>
2023-03-27 12:36:22 +0900

    P10757

---
+++ Baekjoon/src/myPackage/P10757.java
@@ -0,0 +1,26 @@
+package myPackage;
+
+import java.io.*;
+import java.util.StringTokenizer;
+import java.math.*;
+
+public class P10757 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		BigInteger a = new BigInteger(st.nextToken());
+		BigInteger b = new BigInteger(st.nextToken());
+		
+		bw.write(a.add(b) + "");
+		
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+}

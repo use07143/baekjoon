9482760a60d7254caec0de2d9557c781a5eba473
yoon.09 <use07143@daum.net>
2023-03-27 23:30:34 +0900

    P16430

--- Baekjoon/src/myPackage/P16430.java
+++ Baekjoon/src/myPackage/P16430.java
@@ -1,5 +1,35 @@
 package myPackage;
 
+import java.io.*;
+import java.util.StringTokenizer;
+
 public class P16430 {
 
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		
+		int P = B - A;
+		int Q = B;
+		
+		P /= GCD(P, Q);
+		Q /= GCD(P, Q);
+		
+		bw.write(P + " " + Q);
+		
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+	
+	public static int GCD(int a, int b) {
+		if(b == 0) return a;
+		return GCD(b, a%b);
+	}
 }
+

adaaee512d1aad38e889fe6792d5fb6e449b5d35
yoon.09 <use07143@daum.net>
2023-03-27 14:02:36 +0900

    P10872

---
+++ Baekjoon/src/myPackage/P10872.java
@@ -0,0 +1,24 @@
+package myPackage;
+
+import java.io.*;
+
+public class P10872 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int n = Integer.parseInt(br.readLine());
+		bw.write(fac(n) + "");
+		
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+	
+	public static int fac (int n) {
+		if(n == 0) return 1;
+		else return n * fac(n-1);
+	}
+}

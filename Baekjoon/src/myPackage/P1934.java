07cf59a9196c419849c3121c1eef38da784150b2
yoon.09 <use07143@daum.net>
2023-03-27 11:29:02 +0900

    P1934

---
+++ Baekjoon/src/myPackage/P1934.java
@@ -0,0 +1,37 @@
+package myPackage;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class P1934 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int n = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+		int a, b;
+		
+		for(int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			a = Integer.parseInt(st.nextToken());
+			b = Integer.parseInt(st.nextToken());
+			bw.write(LCD(a,b) + "\n");
+		}
+		
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+	
+	public static int GCD (int a, int b) {
+		if(b == 0) return a;
+		else return GCD(b, a % b);
+	}
+	
+	public static int LCD (int a, int b) {
+		return a * b / GCD(a,b);
+	}
+}

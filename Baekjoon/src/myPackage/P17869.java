f0936b5191d2975e8a7fdf8ece8bc58bf1aa384d
yoon.09 <use07143@daum.net>
2023-03-28 01:17:54 +0900

    P17869

--- Baekjoon/src/myPackage/P17869.java
+++ Baekjoon/src/myPackage/P17869.java
@@ -1,5 +1,35 @@
 package myPackage;
 
+import java.io.*;
+
 public class P17869 {
 
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		long n = Long.parseLong(br.readLine());
+		int cnt = 0;
+		
+		bw.write(SCS(n, cnt) + "");
+		
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+	
+	public static int SCS(long n, int cnt) {
+		
+		if(n == 1) return cnt;
+		
+		if(n % 2 == 0) {
+			cnt++;
+			return SCS(n/2, cnt);
+		}
+		else {
+			cnt++;
+			return SCS(n+1, cnt);
+		}
+	}
 }

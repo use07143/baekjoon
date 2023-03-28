cbce89eea33b8d8c58ccc7ecf4d24a6f1c871906
yoon.09 <use07143@daum.net>
2023-03-28 23:16:52 +0900

    P2839

---
+++ Baekjoon/src/myPackage/P2839.java
@@ -0,0 +1,31 @@
+package myPackage;
+
+import java.io.*;
+
+public class P2839 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int n = Integer.parseInt(br.readLine());
+		int min = Integer.MAX_VALUE;
+		
+		for(int x = 0; x <= 1000; x++) {
+			for(int y = 0; y < 1700; y++) {
+				int sum = x * 5 + y * 3;
+				if(sum == n) {
+					if(min > (x+y)) min = (x+y);
+				}
+			}
+		}
+		
+		if(min == Integer.MAX_VALUE) bw.write("-1");
+		else bw.write(min + "");
+		
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+}

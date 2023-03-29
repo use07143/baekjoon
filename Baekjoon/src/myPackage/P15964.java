e6e200179a0d851dbe6ea8cbe2990d568c0253eb
yoon.09 <use07143@daum.net>
2023-03-29 18:18:44 +0900

    P15964

---
+++ Baekjoon/src/myPackage/P15964.java
@@ -0,0 +1,29 @@
+package myPackage;
+
+import java.io.*;
+import java.util.StringTokenizer;
+import java.math.BigInteger;
+
+public class P15964 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		BigInteger A = new BigInteger(st.nextToken());
+		BigInteger B = new BigInteger(st.nextToken());
+		
+		BigInteger N1 = A.add(B);
+		BigInteger N2 = A.subtract(B);
+		BigInteger N = N1.multiply(N2);
+		
+		bw.write(N + "");
+		
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+}

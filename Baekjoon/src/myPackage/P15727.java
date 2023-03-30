a9a71bf2888575e943d56bd3b32aa9f154db12a7
yoon.09 <use07143@daum.net>
2023-03-30 10:19:43 +0900

    P15727

---
+++ Baekjoon/src/myPackage/P15727.java
@@ -0,0 +1,21 @@
+package myPackage;
+
+import java.io.*;
+
+public class P15727 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int n = Integer.parseInt(br.readLine());
+		n -= 1;
+		
+		bw.write(n/5 + 1 + "");
+		
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+}

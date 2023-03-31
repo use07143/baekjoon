4b614ef66d5d18368e31a97f1015670977565ff4
yoon.09 <use07143@daum.net>
2023-03-31 11:30:15 +0900

    P10808

---
+++ Baekjoon/src/myPackage/P10808.java
@@ -0,0 +1,31 @@
+package myPackage;
+
+import java.io.*;
+
+public class P10808 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		String str = br.readLine();
+		int[] arr = new int[26];
+		
+		for(int i = 0; i < str.length(); i++) {
+			for(int j = 0; j < 26; j++) {
+				if(str.charAt(i) == (j+97)) {
+					arr[j]++;
+				}
+			}
+		}
+		
+		for(int i = 0; i < 26; i++) {
+			bw.write(arr[i] + " ");
+		}
+		
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+}

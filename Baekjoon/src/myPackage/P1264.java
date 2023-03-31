2ed116ffa231932170bacc140c38e3442839ce46
yoon.09 <use07143@daum.net>
2023-03-31 11:56:04 +0900

    P1264

---
+++ Baekjoon/src/myPackage/P1264.java
@@ -0,0 +1,37 @@
+package myPackage;
+
+import java.io.*;
+
+public class P1264 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		String str;
+		int cnt;
+		
+		while(true) {
+			str = br.readLine();
+			if(str.equals("#")) break;
+			
+			cnt = 0;
+			
+			for(int i = 0; i < str.length(); i++) {
+				switch(str.charAt(i)) {
+				case 'a': case 'A': case 'e': case 'E': case 'i': case 'I': case 'o': case 'O': case 'u': case 'U':
+					cnt++;
+					break;
+				default:
+					break;
+				}
+			}
+			bw.write(cnt + "\n");
+		}
+		
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+}

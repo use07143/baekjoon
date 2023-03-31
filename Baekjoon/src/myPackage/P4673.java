d3953a5bf2f5469d4e1a2bbc91b0b957997a7838
yoon.09 <use07143@daum.net>
2023-04-01 06:25:21 +0900

    P4673

---
+++ Baekjoon/src/myPackage/P4673.java
@@ -0,0 +1,33 @@
+package myPackage;
+
+import java.io.*;
+
+public class P4673 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		for(int i = 1; i < 10000; i++) {
+			if(self(i) == 0) bw.write(i + "\n");
+		}
+		
+		bw.flush();
+		bw.close();
+	}
+	
+	public static int self(int n) {
+		int sum = 0;
+		for(int i = 1; i < n; i++) {
+			sum = i;
+			int temp = i;
+			while(temp != 0) {
+				sum += (temp % 10);
+				temp /= 10;
+			}
+			
+			if(sum == n) return 1;
+		}
+		return 0;
+	}
+}

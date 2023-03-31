d99f05d87e3c8449971e77e649969b54ba7c5d0c
yoon.09 <use07143@daum.net>
2023-04-01 03:47:09 +0900

    P4153

---
+++ Baekjoon/src/myPackage/P4153.java
@@ -0,0 +1,35 @@
+package myPackage;
+
+import java.io.*;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+public class P4153 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int[] arr = new int[3];
+		StringTokenizer st;
+		
+		while(true) {
+			st = new StringTokenizer(br.readLine());
+			for(int i = 0; i < 3; i++) {
+				arr[i] = Integer.parseInt(st.nextToken());
+			}
+			
+			if(arr[0] == 0 && arr[1] == 0 && arr[2] == 0) break;
+			
+			Arrays.sort(arr);
+			
+			if(Math.sqrt(Math.pow(arr[0], 2) + Math.pow(arr[1], 2)) == arr[2]) bw.write("right\n");
+			else bw.write("wrong\n");
+		}
+		
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+}

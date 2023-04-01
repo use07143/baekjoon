93e07b7192887ce5a640f0672d9ac9bfc79e14fb
yoon.09 <use07143@daum.net>
2023-04-02 03:13:00 +0900

    P1312

---
+++ Baekjoon/src/myPackage/P1312.java
@@ -0,0 +1,31 @@
+package myPackage;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class P1312 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+		
+		int res = A % B;
+		
+		for(int i = 1; i < N; i++) {
+			res *= 10;
+			res %= B;
+		}
+		
+		res *= 10;
+		bw.write(res/B + "");
+		
+		bw.flush();
+		bw.close();
+	}
+}

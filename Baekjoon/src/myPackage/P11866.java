9b1e8aea875585e06a281aef0ce031f45149c880
yoon.09 <use07143@daum.net>
2023-04-02 06:42:52 +0900

    P11866

---
+++ Baekjoon/src/myPackage/P11866.java
@@ -0,0 +1,45 @@
+package myPackage;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class P11866 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		int[] arr = new int[n+1];
+		int count = 1;
+		int temp, now = k;
+		
+		for(int i = 1; i <= n; i++) {
+			arr[i] = i;
+		}
+		
+		bw.write("<" + arr[k]);
+		arr[k] = 0;
+		
+		while(count < n) {
+			temp = 0;
+			while(temp < k) {
+				now++;
+				if(now > n) now = 1;
+				if(arr[now] != 0) temp++;
+			}
+			bw.write(", " + arr[now]);
+			arr[now] = 0;
+			count++;
+		}
+		
+		bw.write(">");
+		
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+}

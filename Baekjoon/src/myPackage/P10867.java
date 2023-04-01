ca7e56b4a3cab7d8782e2fa421852ac447d0cbaf
yoon.09 <use07143@daum.net>
2023-04-02 01:52:13 +0900

    P10867

---
+++ Baekjoon/src/myPackage/P10867.java
@@ -0,0 +1,36 @@
+package myPackage;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class P10867 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		for(int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		
+		bw.write(arr[0] + " ");
+		
+		for(int i = 1; i < n; i++) {
+			if(arr[i-1] == arr[i]) continue;
+			
+			bw.write(arr[i] + " ");
+		}
+		
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+}

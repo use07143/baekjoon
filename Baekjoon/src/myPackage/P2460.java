088a851f07809054db5623eba69926f1d3079bc7
yoon.09 <use07143@daum.net>
2023-04-02 00:06:42 +0900

    P2460

---
+++ Baekjoon/src/myPackage/P2460.java
@@ -0,0 +1,30 @@
+package myPackage;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class P2460 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st;
+		int num = 0;
+		int max = 0;
+		
+		for(int i = 0; i < 10; i++) {
+			st = new StringTokenizer(br.readLine());
+			num -= Integer.parseInt(st.nextToken());
+			num += Integer.parseInt(st.nextToken());
+			if(num > max) max = num;
+		}
+		
+		bw.write(max + "");
+		
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+}

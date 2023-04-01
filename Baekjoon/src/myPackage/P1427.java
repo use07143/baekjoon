cc57478a073b31ec1745837b252f6019c81ab179
yoon.09 <use07143@daum.net>
2023-04-02 06:17:31 +0900

    P1427

---
+++ Baekjoon/src/myPackage/P1427.java
@@ -0,0 +1,31 @@
+package myPackage;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class P1427 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		String str = br.readLine();
+		
+		int[] arr = new int[str.length()];
+		
+		for(int i = 0; i < str.length(); i++) {
+			arr[i] = Integer.parseInt(String.valueOf(str.charAt(i)));
+		}
+		
+		Arrays.sort(arr);
+		
+		for(int i = str.length()-1; i >= 0; i--) {
+			bw.write(arr[i] + "");
+		}
+		
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+}

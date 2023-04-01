dbd3e28b53831df27a0286c2ccaa620da71cbba4
yoon.09 <use07143@daum.net>
2023-04-02 01:57:50 +0900

    P11004

---
+++ Baekjoon/src/myPackage/P11004.java
@@ -0,0 +1,32 @@
+package myPackage;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class P11004 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		long[] arr = new long[n];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < n; i++) {
+			arr[i] = Long.parseLong(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		
+		bw.write(arr[k-1] + "");
+		
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+}

c490448ead33250eacdd1757f2b92d3581e67ff2
yoon.09 <use07143@daum.net>
2023-04-04 11:52:20 +0900

    P2914

---
+++ Baekjoon/src/myPackage/P2914.java
@@ -0,0 +1,24 @@
+package myPackage;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class P2914 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		
+		int n = a * (b - 1) + 1;
+		bw.write(n + "");
+		
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+}

5a5a97b2d7846d0b13a54f57db4e527390c6d9e0
yoon.09 <use07143@daum.net>
2023-04-04 12:12:28 +0900

    P2935

---
+++ Baekjoon/src/myPackage/P2935.java
@@ -0,0 +1,25 @@
+package myPackage;
+
+import java.io.*;
+import java.math.BigInteger;
+
+public class P2935 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		BigInteger n1 = new BigInteger(br.readLine());
+		String str = br.readLine();
+		BigInteger n2 = new BigInteger(br.readLine());
+		
+		if(str.charAt(0) == '*') bw.write(n1.multiply(n2) + "");
+		else if(str.charAt(0) == '+') bw.write(n1.add(n2) + "");
+		
+		
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+}

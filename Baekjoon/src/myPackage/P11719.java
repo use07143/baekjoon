643644908fc4c970806d0fe572b149d5688fa52f
yoon.09 <use07143@daum.net>
2023-04-11 12:06:56 +0900

    P11719

---
+++ Baekjoon/src/myPackage/P11719.java
@@ -0,0 +1,22 @@
+package myPackage;
+
+import java.io.*;
+
+public class P11719 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		String str ;
+		
+		while((str = br.readLine()) != null) {			
+			bw.write(str + "\n");
+		}
+		
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+}

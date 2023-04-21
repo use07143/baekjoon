8b4ea41f7edc189c2f95aacd2216abfe4e7ab09c
y00n.09 <75162689+use07143@users.noreply.github.com>
2023-04-21 11:12:36 +0900

    P11653 - 소인수분해

--- Baekjoon/src/myPackage/P11653.java
+++ Baekjoon/src/myPackage/P11653.java
@@ -14,7 +14,6 @@ public class P11653 {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		int N = Integer.parseInt(br.readLine());
-		
 		while(N != 1) {
 			for(int i = 2; i <= N; i++) {
 				if(N % i == 0) {

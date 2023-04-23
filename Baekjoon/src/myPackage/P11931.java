ae0bcaa0aaba025716e8b272080aa025b43d4ba6
y00n.09 <75162689+use07143@users.noreply.github.com>
2023-04-23 17:49:08 +0900

    P11931 - 수 정렬하기 4

--- Baekjoon/src/myPackage/P11931.java
+++ Baekjoon/src/myPackage/P11931.java
@@ -12,7 +12,6 @@ public class P11931 {
 		
 		int n = Integer.parseInt(br.readLine());
 		int[] arr = new int[n];
-		
 		for(int i = 0; i < n; i++) {
 			arr[i] = Integer.parseInt(br.readLine());
 		}
